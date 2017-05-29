13a3975e73332690973ba323782035fdab8a52b1
John Bendfeldt <jfbendfeldt@gmail.com>
2017-05-29 17:58:21 -0500

    added js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAWZf9g3SKBLeJP10gjBxfeDwbrfKaTRQY",
+    authDomain: "bookapp-e9795.firebaseapp.com",
+    databaseURL: "https://bookapp-e9795.firebaseio.com",
+    projectId: "bookapp-e9795",
+    storageBucket: "bookapp-e9795.appspot.com",
+    messagingSenderId: "216382304423"
+  };
+  firebase.initializeApp(config);
